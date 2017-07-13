e38ba568a9f8d9e43113e34df12175081d86486e
chraigui soufiene <s.chraigui@gmail.com>
2017-07-13 03:26:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,12 +25,16 @@ app.use(bodyParser.json())
 
 // index
 app.get('/', function (req, res) {
-	//var url = require('url');
-	var url_parts = request;
-	//var url_parts = url.parse(request.url, true);
-	//var query = url_parts.query;
-	//var hub_challenge = req.query.hub_challenge;
-	res.send(url_parts);
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'GET',
+		json: {
+			hub_challenge: {hub_challenge:hub_challenge},
+			//message: messageData,
+		}
+	res.send(22222222);
+	}
 })
 
 // for facebook verification
